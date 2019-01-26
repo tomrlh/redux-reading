3489022f5465a3e221c9536aee80a760fc0abeb1
Thigo Medeiros <si-thiago@hotmail.com>
2019-01-26 01:15:18 -0300

    Restarting project with a different UI component lib (Semantic UI)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Button } from 'semantic-ui-react'
+import { Grid, Image, Segment, Container } from 'semantic-ui-react'
+import logo from './logo.svg'
+import paragraph from './paragraph.png'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Container>
+					<Grid columns='equal'>
+						<Grid.Column>
+							<Segment>Categories</Segment>
+						</Grid.Column>
+						<Grid.Column width={10}>
+							<Segment>Posts</Segment>
+						</Grid.Column>
+					</Grid>
+				</Container>
+			</div>
+		);
+	}
 }
 
 export default App;
